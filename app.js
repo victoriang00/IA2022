5963f4f725f645636c8641d0784eac72f4808630
victoriang00 <victoriang263@gmail.com>
2021-04-16 08:05:35 +0800

    push

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+// The core Firebase JS SDK is always required and must be listed first
+src = "https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js";
+
+//TODO: Add SDKs for Firebase products that you want to use https://firebase.google.com/docs/web/setup#available-libraries
+src = "https://www.gstatic.com/firebasejs/8.4.1/firebase-analytics.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCHwwp-hRiL8ohdDpWOHIVN9wNYE9q5TXI",
+  authDomain: "ia2022.firebaseapp.com",
+  projectId: "ia2022",
+  storageBucket: "ia2022.appspot.com",
+  messagingSenderId: "318603254564",
+  appId: "1:318603254564:web:89865e39a88f1bf9d7f1b1",
+  measurementId: "G-X78PMBTXKB",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
